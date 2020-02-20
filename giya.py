7a4ad7dddc8887fce07b5c0b987c1f3f94f89582
Zeth <zeth.leonardo@protonmail.com>
2020-02-21 03:33:34 +0400

    Blacked ;)

--- giya.py
+++ giya.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
