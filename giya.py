699e103c475cdc57acd3f7b164bcd10493a5d40b
Zeth <zeth.leonardo@protonmail.com>
2020-02-21 03:33:19 +0400

    Added dotenv loader.

--- giya.py
+++ giya.py
@@ -1,8 +1,15 @@
 import os
 
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 from flask_migrate import Migrate
 
 from app import create_app, db
+
 
 # Import blueprints
 
