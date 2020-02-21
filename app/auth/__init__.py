02b7c20ab085c57612fcb1b7dd4044a0690a17f4
Zeth <zeth.leonardo@protonmail.com>
2020-02-21 04:01:02 +0400

    Created Auth Blueprint and API.

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,1 +1,14 @@
-# Entry point
+from flask_restx import Api
+from flask import Blueprint
+
+# Import auth namespace
+from .controller import api as auth_ns
+
+auth_bp = Blueprint("auth", __name__)
+
+auth = Api(
+    auth_bp, title="Authenticate", description="Authenticate and receive tokens."
+)
+
+# API.
+auth.add_namespace(auth_ns)
