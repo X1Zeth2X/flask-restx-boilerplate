bc26c42a58edfd5b749fb6ee36cd7396658719ec
Zeth <zeth.leonardo@protonmail.com>
2020-02-22 19:21:56 +0400

    Add user get 404 test.

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -36,3 +36,7 @@ class TestUserBlueprint(BaseTestCase):
         self.assertTrue(user_resp.status)
         self.assertEquals(user_resp.status_code, 200)
         self.assertEquals(user_data["user"]["username"], username)
+
+        # Test a 404 request
+        user_404_resp = get_user_data(self, access_token, "non.existent")
+        self.assertEquals(user_404_resp.status_code, 404)
