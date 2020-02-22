4b96eb3d4911bfc4ee615c04636243bb60f379e9
Zeth <zeth.leonardo@protonmail.com>
2020-02-22 13:28:18 +0400

    Added common response utils.

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,21 @@
-def Message(status, message):
+def message(status, message):
     response_object = {"status": status, "message": message}
+    return response_object
+
+
+def validation_error(status, errors):
+    response_object = {"status": status, "errors": errors}
 
     return response_object
+
+
+def err_resp(msg, reason, code):
+    err = message(False, msg)
+    err["error_reason"] = reason
+    return err, code
+
+
+def internal_err_resp():
+    err = message(False, "Something went wrong during the process!")
+    err["error_reason"] = "server_error"
+    return err, 500
