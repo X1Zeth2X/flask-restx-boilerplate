5589af17ed0b4369f9457b460acc877387f08ad0
Zeth <zeth.leonardo@protonmail.com>
2020-02-22 19:07:24 +0400

    Fixed specific unit testing.

--- giya.py
+++ giya.py
@@ -6,8 +6,10 @@ dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from flask_migrate import Migrate
+##
 
+import click
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models.user import User, Role, Permission
 
@@ -26,16 +28,23 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
-
-    # Import unittest
+@click.argument("test_names", nargs=-1)
+def test(test_names):
+    """ Run unit tests """
     import unittest
 
-    # Runs unit tests
+    if test_names:
+        """ Run specific unit tests.
 
-    tests = unittest.TestLoader().discover("tests", pattern="test*.py")
+        Example:
+        $ flask test tests.test_auth_api tests.test_user_model ...
+        """
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+
+    else:
+        tests = unittest.TestLoader().discover("tests", pattern="test*.py")
+
     result = unittest.TextTestRunner(verbosity=2).run(tests)
-
     if result.wasSuccessful():
         return 0
 
