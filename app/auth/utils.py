7f8001a2cdcf7a3a0950464956a99821e8c97ff9
Zeth <zeth.leonardo@protonmail.com>
2020-02-22 13:28:54 +0400

    Added login/register schemas for validations.

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,8 +1,4 @@
 from flask_restx import Namespace, fields
-from re import compile
-
-# Basic Email REGEX
-EMAIl_REGEX = compile(r"[^@]+@[^@]+\.[^@]+")
 
 
 class AuthDto:
@@ -47,3 +43,51 @@ class AuthDto:
             "user": fields.Nested(user_obj),
         },
     )
+
+
+# Validations
+from marshmallow import Schema, fields
+from marshmallow.validate import Regexp, Length, Email
+
+
+class LoginSchema(Schema):
+    """ /auth/login [POST]
+
+    Parameters:
+    - Email
+    - Password (Str)
+    """
+
+    email = fields.Email(required=True, validate=[Length(max=64)])
+    password = fields.Str(required=True, validate=[Length(min=8, max=128)])
+
+
+class RegisterSchema(Schema):
+    """ /auth/register [POST]
+
+    Parameters:
+    - Email
+    - Username (Str)
+    - Name (Str)
+    - Password (Str)
+    """
+
+    email = fields.Email(required=True, validate=[Length(max=64)])
+    username = fields.Str(
+        required=True,
+        validate=[
+            Length(min=4, max=15),
+            Regexp(
+                r"^([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)$",
+                error="Invalid username!",
+            ),
+        ],
+    )
+    name = fields.Str(
+        validate=[
+            Regexp(
+                r"^[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*$", error="Invalid name!",
+            )
+        ]
+    )
+    password = fields.Str(required=True, validate=[Length(min=8, max=128)])
