9cb50f35d8483ab7a7f1e46bab0b500035282837
Zeth <zeth.leonardo@protonmail.com>
2020-02-22 19:08:38 +0400

    Add self.client for making requests.

---
+++ tests/utils/base.py
@@ -0,0 +1,17 @@
+import unittest
+from app import db, create_app
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("test")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
