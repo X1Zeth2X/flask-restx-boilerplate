1f83926ec8d13805451636cd8fce47aabde1dbb0
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-09-30 23:50:02 +0400

    Update `ModelSchema` to `Schema`.

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -4,7 +4,7 @@ from app import ma
 from .user import User
 
 
-class UserSchema(ma.ModelSchema):
+class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose, add more if needed.
         fields = ("email", "name", "username", "joined_date", "role_id")
